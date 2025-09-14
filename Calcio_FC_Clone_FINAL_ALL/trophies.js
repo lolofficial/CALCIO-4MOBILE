aee8ef86c7c5e3937ad8563cf75e491086af7f1f
lolofficial <lolgamer12_yt@proton.me>
2025-09-14 10:42:43 +0200

    Delete Calcio_FC_Clone_FINAL_ALL directory

--- Calcio_FC_Clone_FINAL_ALL/trophies.js
+++
@@ -1,9 +0,0 @@
-
-function renderTrophies() {
-  const div = document.getElementById("trophyBox");
-  if (!div) return;
-  const trophy = localStorage.getItem("tournamentTrophy");
-  div.innerHTML = "<h3>Trofei sbloccati:</h3>";
-  if (trophy) div.innerHTML += "<p>" + trophy + "</p>";
-  else div.innerHTML += "<p>Nessun trofeo sbloccato.</p>";
-}
